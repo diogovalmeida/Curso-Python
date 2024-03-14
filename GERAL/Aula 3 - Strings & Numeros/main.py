6a0cdf3914b2b5333b3d572ddae2c84f8205377c
Diogo Almeida <diogovazalmeida@gmail.com>
2024-03-14 12:29:06 +0000

    Aula 1/2/3

---
+++ GERAL/Aula 3 - Strings & Numeros/main.py
@@ -0,0 +1,12 @@
+# Text: String = str('texto')
+
+# Numbers: Integer = Inteiro (1, 2, 3)
+# Numbers: Float = Fração (3.5, 4.7, 10.6)
+
+# Boolean Type: Bool (True or False)
+
+x=2
+y='ola'
+
+print(x + x)
+print(y)
