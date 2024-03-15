57f20a252021820e8a7dbffaa8b5e9b8d606aaff
Diogo Almeida <diogovazalmeida@gmail.com>
2024-03-15 21:51:06 +0000

    Aula 3/4/5/6/7/8/9

--- GERAL/Aula 3 - Strings & Numeros/main.py
+++ GERAL/Aula 3 - Strings & Numeros/main.py
@@ -8,5 +8,25 @@
 x=2
 y='ola'
 
-print(x + x)
+#print(x + x)
+#print(y)
+
+# MODIFICANDO O TIPO DE DADOS
+
+x = str(3)
+y = int(4)
+z = float(5)
+
+print(x)
+print(x+x)
 print(y)
+print(z)
+
+# O Diogo tem 21 anos de idade e mora na cidade de Lisboa
+
+nome = 'Diogo'
+idade = 21
+idade = str(idade)
+cidade = 'Lisboa'
+# Ou fazemos como está em cima ou como está em baixo
+print('O ' + nome + ' tem ' + str(idade) + ' anos de idade e mora na cidade de ' + cidade + '.')
