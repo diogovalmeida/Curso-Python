47963c16440b7fa307ed08ce143aad02e4e44d6a
Diogo Almeida <diogovazalmeida@gmail.com>
2024-03-15 21:52:26 +0000

    Aula 3/4/5/6/7/8/9

---
+++ GERAL/Aula 4 - Slice/main.py
@@ -0,0 +1,14 @@
+fruta = 'Abacate'
+#index = 0123456
+
+print(fruta)
+print(fruta[3])
+print(fruta[2:6])
+print(fruta[-1]) # Conta de trás para a frente
+
+
+valor = 99.75
+valor = str(valor) # Obrigatório colocar que é string
+#index  01234
+
+print(valor[:2])
---
+++ GERAL/Aula 7 - Operações/main.py
@@ -0,0 +1,43 @@
+# OPERAÇÕES MATEMÁTICAS
+
+calculo = 2 + 2
+calculo2 = 2 + 2 * 3
+calculo3 = 2 + 2 * 3 / 2
+calculo4 = (2 + 2) * 4
+calculo5 = 2 ** 3
+
+print(calculo)
+print(calculo2)
+print(calculo3)
+print(calculo4)
+print(calculo5)
+
+# OPERADORES DE COMPARAÇÃO
+
+'''
+== igual
+!= não igual
+> maior que
+< menor que
+>= maior ou igual a
+<= menor ou igual a
+'''
+
+# Dá para fazer também com texto
+operadores = 10 == 10
+operadores2 = 11 == 20
+
+print(operadores)
+print(operadores2)
+
+# OPERADORES DE ATRIBUIÇÃO
+
+x = 10 
+#x = x + 5
+#x += 5
+#x -= 5
+#x *= 5
+#x /= 5
+x %= 3 # É o resto -> Posso colocar três nº's 3 dentro de 10 e sobra 1 
+
+print(x)
