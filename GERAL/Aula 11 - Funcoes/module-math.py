76b06f1e0e7809e94737ab44c915778199c900d1
Diogo Almeida <diogovazalmeida@gmail.com>
2024-03-16 14:55:43 +0000

    Alteração V2

--- GERAL/Aula 11 - Funcoes/module-math.py
+++
@@ -1,9 +0,0 @@
-# Qual é o nº fatorial de 4?
-# 4 * 3 * 2 * 1 = 24
-
-import math     # Adicionar módulo de matemática
-
-print(math.factorial(4))
-
-fatorial = 4 * 3 * 2 * 1
-print(fatorial)
--- GERAL/Aula 8 - Declarações Condicionadas/main.py
+++
@@ -1,9 +0,0 @@
-velocidade = 100
-
-if velocidade > 110:
-    print('Acima da Velocidade permitida')
-    print('Por favor reduzir a sua velocidade')
-elif velocidade < 60:
-    print('Por favor dirigir acima de 80Km/h')
-else:
-    print('Velocidade OK')
